4e455f193188b54f33ad5954aee55b8b677df7b7
akshayDeq <akshay@decode.com>
2021-12-22 16:37:06 +0530

    implement todo service functions

---
+++ src/todo/todo.mock.data.ts
@@ -0,0 +1,7 @@
+export const TODOS = [
+  { id: 1, title: ['buy grocery'] },
+  { id: 2, title: ['clean room'] },
+  { id: 3, title: ['go to bank'] },
+  { id: 4, title: ['order food'] },
+  { id: 5, title: ['pay electricity bill'] },
+];
---
+++ src/todo/todo.service.ts
@@ -0,0 +1,50 @@
+import { HttpException, Injectable } from '@nestjs/common';
+// import { resolve } from 'path/posix';
+import { TODOS } from './todo.mock.data';
+
+@Injectable()
+export class TodoService {
+  todos = TODOS;
+
+  getTodos(): Promise<any> {
+    return new Promise((resolve) => {
+      resolve(this.todos);
+    });
+  }
+
+  getTodo(todoId): Promise<any> {
+    return new Promise((resolve) => {
+      const todo = this.todos.find((todo) => todo.id == todoId);
+      if (!todo) {
+        throw new HttpException(`No todo with ${todoId} ID exists`, 404);
+      }
+      resolve(todo);
+    });
+  }
+
+  postTodo(newTodo: { id: number; title: string[] }): Promise<any> {
+    return new Promise((resolve) => {
+      const checkForUniqueID = this.todos.find(
+        (todo) => todo.id === newTodo.id,
+      );
+      if (checkForUniqueID) {
+        throw new HttpException(`ID already exists`, 404);
+      }
+      this.todos.push(newTodo);
+      resolve(this.todos);
+    });
+  }
+
+  deleteTodo(id: number): Promise<any> {
+    return new Promise((resolve) => {
+      const checkIfIdExists = this.todos.find((todo) => todo.id === id);
+      if (!checkIfIdExists) {
+        throw new HttpException(`No such ID exists`, 404);
+      }
+      this.todos = this.todos.filter((todo) => {
+        todo.id !== id;
+      });
+      resolve(this.todos);
+    });
+  }
+}
