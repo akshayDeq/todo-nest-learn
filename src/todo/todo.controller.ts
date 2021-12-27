0e7cf55457b098b703d6f570a61cc056fc9f4ae1
akshay <akanojiya@deqode.com>
2021-12-27 15:32:39 +0530

    refactor todo controller code

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -3,72 +3,34 @@ import {
   Controller,
   Delete,
   Get,
-  InternalServerErrorException,
   Param,
   ParseIntPipe,
-  Patch,
   Post,
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
-import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
-  async getTodos() {
-    try {
-      const todos = await this.todoService.getTodos();
-      return todos;
-    } catch (error) {
-      throw new InternalServerErrorException();
-    }
+  getTodos() {
+    return this.todoService.getTodos();
   }
 
   @Get(':todoId')
-  async getTodo(@Param('todoId', ParseIntPipe) todoId: number) {
-    try {
-      const todo = await this.todoService.getTodo(todoId);
-      return todo;
-    } catch (error) {
-      throw new InternalServerErrorException({
-        description: `No todo with id:${todoId} found`,
-      });
-    }
+  getTodo(@Param('todoId', ParseIntPipe) todoId: number) {
+    return this.todoService.getTodo(todoId);
   }
 
   @Post()
-  async postTodo(@Body() createTodoDto: CreateTodoDto) {
-    try {
-      const todos = await this.todoService.postTodo(createTodoDto);
-      return todos;
-    } catch (error) {
-      throw new InternalServerErrorException();
-    }
+  postTodo(@Body() createTodoDto: CreateTodoDto) {
+    return this.todoService.postTodo(createTodoDto);
   }
 
   @Delete(':todoId')
   async deleteTodo(@Param('todoId', ParseIntPipe) todoId: number) {
-    try {
-      const todos = await this.todoService.deleteTodo(todoId);
-      return todos;
-    } catch (error) {
-      throw new InternalServerErrorException();
-    }
-  }
-
-  @Patch(':todoId')
-  async addTodoItems(
-    @Param('todoId', ParseIntPipe) todoId: number,
-    @Body() todoItems: UpdateTodoDto[],
-  ) {
-    try {
-      const todos = await this.todoService.addMoreTodoItems(todoId, todoItems);
-      return todos;
-    } catch (error) {
-      throw new InternalServerErrorException();
-    }
+    return this.todoService.deleteTodo(todoId);
   }
 }
