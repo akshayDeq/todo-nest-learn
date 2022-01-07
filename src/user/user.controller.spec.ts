66440406dcbe859eaebe2cca8acde2cba57e04d2
akshay <akanojiya@deqode.com>
2022-01-07 19:21:04 +0530

    test for user service and controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -11,9 +11,22 @@ import { UserService } from './user.service';
 describe('user controller unit tests', () => {
   let userController: UserController;
   let spyService: UserService;
-  let usersRepository: Repository<Users>;
+  // let usersRepository: Repository<Users>;
 
-  const mockUserService = {};
+  const mockUserService = {
+    getUsers: jest.fn(() => {
+      return [{}];
+    }),
+    createUser: jest.fn((dto) => {
+      return { id: Date.now(), username: dto.username, password: dto.password };
+    }),
+    getUserById: jest.fn((id) => {
+      return { id, username: 'akshay', password: 'hashpass' };
+    }),
+    deleteUser: jest.fn((id) => {
+      return { message: `userId:${id} deleted successfully` };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -35,12 +48,39 @@ describe('user controller unit tests', () => {
 
     spyService = module.get<UserService>(UserService);
     userController = module.get<UserController>(UserController);
-    usersRepository = module.get<Repository<Users>>(getRepositoryToken(Users));
+    // usersRepository = module.get<Repository<Users>>(getRepositoryToken(Users));
   });
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
     expect(spyService).toBeDefined();
-    expect(usersRepository).toBeDefined();
+  });
+
+  it('should return all users', () => {
+    expect(userController.getUsers()).toEqual([{}]);
+    expect(mockUserService.getUsers).toHaveBeenCalled();
+  });
+
+  it('should create a user', () => {
+    expect(
+      userController.createUser({ username: 'akshay', password: 'hashpass' }),
+    ).toEqual({ id: Date.now(), username: 'akshay', password: 'hashpass' });
+    expect(mockUserService.createUser).toHaveBeenCalled();
+  });
+
+  it('should return a user by id', () => {
+    expect(userController.getUserById(1)).toEqual({
+      id: 1,
+      username: 'akshay',
+      password: 'hashpass',
+    });
+    expect(mockUserService.getUserById).toHaveBeenCalledWith(1);
+  });
+
+  it('should delete a user by id', () => {
+    expect(userController.deleteUser(1)).toEqual({
+      message: `userId:1 deleted successfully`,
+    });
+    expect(mockUserService.deleteUser).toHaveBeenCalledWith(1);
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,13 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Users } from '../entities/user.entity';
-import { Repository } from 'typeorm';
 import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { BcryptService } from '../utilities/bcrypt/bcrypt.utility';
+import { Repository } from 'typeorm';
+import { ConflictException } from '@nestjs/common';
 
 describe('user service unit tests', () => {
-  let spyService: UserService;
-  let userRepository: Repository<Users>;
+  let service: UserService;
+  let userRepo: Repository<Users>;
+
+  const mockUser = {
+    username: 'akshaytest',
+    password: 'akshaytest',
+  };
+
+  const mockUsers = [{ id: 1, ...mockUser }];
+
+  const mockRepo = {
+    insert: jest.fn(),
+    find: jest.fn(() => {
+      return mockUsers;
+    }),
+    findOne: jest.fn(({ where: { field } }) => {
+      switch (field) {
+        case 1:
+        case 'akshaytest':
+          return { ...mockUser };
+        default:
+          return null;
+      }
+    }),
+    delete: jest.fn(({ id }) => {
+      if (id == 1) {
+        return { message: 'User deleted successfully' };
+      }
+      return null;
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -15,17 +46,44 @@ describe('user service unit tests', () => {
         BcryptService,
         {
           provide: getRepositoryToken(Users),
-          useClass: Repository,
+          useValue: mockRepo,
         },
       ],
     }).compile();
 
-    spyService = module.get<UserService>(UserService);
-    userRepository = module.get<Repository<Users>>(getRepositoryToken(Users));
+    service = module.get<UserService>(UserService);
+    userRepo = module.get<Repository<Users>>(getRepositoryToken(Users));
   });
 
   it('should be defined', () => {
-    expect(spyService).toBeDefined();
-    expect(userRepository).toBeDefined();
+    expect(service).toBeDefined();
+    expect(userRepo).toBeDefined();
+  });
+
+  it('should find a user by id', async () => {
+    expect(await service.getUserById(1)).toEqual({ id: 1, ...mockUser });
+  });
+
+  it('should get all users in database', async () => {
+    expect(await service.getUsers()).toEqual(mockUsers);
+  });
+
+  it('should check if user already exists', async () => {
+    expect(await service.checkUserAlreadyExists('akshaytest')).toEqual(false);
+  });
+
+  it('should insert a new user in database', async () => {
+    expect(
+      await service.createUser({
+        username: 'akshaytest',
+        password: 'akshaypass',
+      }),
+    ).toEqual(new ConflictException({ message: 'User already exists' }));
+  });
+
+  it('should delete a user', async () => {
+    expect(await service.deleteUser(1)).toEqual({
+      message: 'User deleted successfully',
+    });
   });
 });
