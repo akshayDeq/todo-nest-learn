5a162489709aa7dd1593a155ae62918316c151a4
akshay <akanojiya@deqode.com>
2022-01-12 12:18:42 +0530

    Update : include users entity in todo module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { Todo } from 'src/entities/todo.entity';
+import { Users } from 'src/entities/user.entity';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Todo]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Todo, Users]), AuthModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
